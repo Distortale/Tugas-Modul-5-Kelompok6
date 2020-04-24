14e79e6903d8bd9cb3855c61b93d21d2639f0a34
SaifudinNA <apin99lightning@gmail.com>
2020-04-24 22:45:53 +0700

    Revert "Revert "Perubahan Beberapa Naming Violation""
    This reverts commit a9348ae1986e5cefaf5b734cdb92c7d228d5ef90.

--- Modul5_Tugas_Kelompok6/Modul5_Tugas_Kelompok6/userService.cs
+++ Modul5_Tugas_Kelompok6/Modul5_Tugas_Kelompok6/userService.cs
@@ -6,28 +6,28 @@ using System.Threading.Tasks;
 
 namespace Modul5_Kelompok6_CSharp
 {
-    class userService
+    class UserService
     {
         private string[,] data, histories;
         private string email, password, roles, buku1, buku2, tanggal = "";
 
-        public userService(string emails, string passwords)
+        public UserService(string emails, string passwords)
         {
             email = emails;
             password = passwords;
             data = new string[2, 3] {
-                {"kanyakelompok6@gmail.com", "12345", "superadmin" },
-                {"alfinkelompok6@gmail.com", "12345", "user"  }
+                {"kanyakelompok06@gmail.com", "12345", "superadmin" },
+                {"alfinkelompok06@gmail.com", "12345", "user"  }
             };
             histories = new string[2, 4] {
-                {"kanyakelompok6@gmail.com", "Fisika Dasar", "Dasar Komputer dan Pemprograman", "24-04-20" },
-                {"alfinkelompok6@gmail.com", "Dasar Komputer dan Pemprograman", "Konsep Jaringan Komputer", "24-04-20"  }
+                {"kanyakelompok06@gmail.com", "Fisika Dasar", "Dasar Komputer dan Pemprograman", "24-04-20" },
+                {"alfinkelompok06@gmail.com", "Dasar Komputer dan Pemprograman", "Konsep Jaringan Komputer", "24-04-20"  }
             };
         }
 
-        public void login()
+        public void Login()
         {
-            var (status, role) = checkCredentials();
+            var (status, role) = CheckCredentials();
             if (status == true)
             {
                 Console.WriteLine("\nWelcome " + role);
@@ -38,24 +38,24 @@ namespace Modul5_Kelompok6_CSharp
                 Console.WriteLine("\nInvalid Login");
             }
         }
-        public void pinjaman()
+        public void Pinjaman()
         {
-            var (status, buku1) = checkBooks1();
+            var (status, buku1) = CheckBooks1();
             if (status == true)
             {
                 Console.WriteLine("\n" + email + " Meminjam buku : \n" + buku1);
             }
         }
-        public void waktu()
+        public void Waktu()
         {
-            var (status, tanggal) = checkTime();
+            var (status, tanggal) = CheckTime();
             if (status == true)
             {
                 Console.WriteLine("Tanggal Peminjaman : " + tanggal);
             }
 
         }
-        private (bool, string) checkCredentials()
+        private (bool, string) CheckCredentials()
         {
             for (int i = 0; i < histories.GetLength(0); i++)
             {
@@ -67,7 +67,7 @@ namespace Modul5_Kelompok6_CSharp
             }
             return (false, roles);
         }
-        private (bool, string) checkBooks1()
+        private (bool, string) CheckBooks1()
         {
             for (int i = 0; i < histories.GetLength(0); i++)
             {
@@ -79,7 +79,7 @@ namespace Modul5_Kelompok6_CSharp
             }
             return (false, buku1);
         }
-        private (bool, string) checkTime()
+        private (bool, string) CheckTime()
         {
             for (int i = 0; i < data.GetLength(0); i++)
             {
